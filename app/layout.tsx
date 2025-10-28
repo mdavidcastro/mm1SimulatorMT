d8017a4ffb23d2779b42dff32735c895e7679425
Manuel <manolo0726castro@gmail.com>
2025-10-28 08:00:46 -0500

    cambio de nombre

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'MM1 Parcial IO2',
   description: 'Created with v0',
   generator: 'v0.app',
 }
